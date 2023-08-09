504af037643af9397f117088a9daa55429746378
mandoo1229 <123732776+mandoo1229@users.noreply.github.com>
2023-08-09 22:27:52 +0900

    Style : 위치를 조정하였습니다.

--- src/page/Navigation.jsx
+++ src/page/Navigation.jsx
@@ -1,6 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import '../style/nav.css';
 
 function Navigation() {
   return (
@@ -9,7 +10,9 @@ function Navigation() {
         <Container>
           <Navbar.Brand href="#home">Minwoo Portfolio</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#home" className="home">
+              Home
+            </Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
