11b7890793944915ea5461bb650c3d1ec7505647
mandoo1229 <123732776+mandoo1229@users.noreply.github.com>
2023-08-09 22:00:02 +0900

    feat : nav 컴포넌트를 다른데로 빼서 main컴포넌트에 적용할 수 있또록 하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Navigation from './page/Navigation';
+import Main from './page/Main';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navigation />} />
+          <Route path="/" element={<Main />} />
         </Routes>
       </BrowserRouter>
     </div>
